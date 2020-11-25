4df05e4ab5bf8f473123a7f42abc643f04892eea
limmens <66556410+limmens@users.noreply.github.com>
2020-11-25 12:45:38 +0100

    Options
    Added Options Menu, which displays the key settings for the game.

---
+++ src/tilegame/ui/UITextButton.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tilegame.ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import tilegame.gfx.Assets;
+import tilegame.gfx.Text;
+
+/**
+ *
+ * @author Loes Immens
+ */
+public class UITextButton extends UIObject
+{
+    private BufferedImage[] images;
+    private ClickListener clicker;
+    private String text;
+
+    public UITextButton(float x, float y, int width, int height, BufferedImage[] images, String text, ClickListener clicker) 
+    {
+        super(x, y, width, height);
+        
+        this.images = images;
+        this.clicker = clicker;
+        this.text = text;
+    }
+
+    @Override
+    public void tick(){}
+
+    @Override
+    public void render(Graphics g) 
+    {
+        int textX = (int)x + width / 2;
+        int textY = (int)y + height / 2;
+        
+        if(hovering)
+        {
+            g.drawImage(images[1], (int) x, (int) y, width, height, null); //emphasized button image
+            Text.drawString(g, text, textX, textY, true, Color.cyan, Assets.getAssets().font28);
+        }
+        else
+        {
+            g.drawImage(images[0], (int) x, (int) y, width, height, null); //regular button image
+            Text.drawString(g, text, textX, textY, true, Color.black, Assets.getAssets().font28);
+        }
+    }
+
+    @Override
+    public void onClick() 
+    {
+        clicker.onClick();
+    }
+    
+}
