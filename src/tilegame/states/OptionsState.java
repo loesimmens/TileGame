865a98f24af1dc87fa10b67e20aa92e4a1a7c042
limmens <66556410+limmens@users.noreply.github.com>
2020-11-25 12:44:37 +0100

    Added Options Menu, which displays the key settings for the game.

--- src/tilegame/states/OptionsState.java
+++ src/tilegame/states/OptionsState.java
@@ -5,8 +5,13 @@
  */
 package tilegame.states;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import tilegame.Game;
+import tilegame.gfx.Assets;
+import tilegame.gfx.Text;
 
 /**
  *
@@ -14,10 +19,14 @@ import tilegame.Game;
  */
 public class OptionsState extends State
 {
+    private final int leftX = game.getWidth() / 3;
+    private final int rightX = 2 * game.getWidth() / 3;
+    private LinkedHashMap<String, String> keySettings;
     
     public OptionsState(Game game)
     {
         super(game);
+        setKeySettings();
     }
 
     @Override
@@ -29,7 +38,23 @@ public class OptionsState extends State
     @Override
     public void render(Graphics g) 
     {
-        
+        int y = 64;
+        for(Map.Entry me : keySettings.entrySet())
+        {
+            Text.drawString(g, (String)me.getKey(), leftX, y, true, Color.black, Assets.getAssets().font14);
+            Text.drawString(g, (String)me.getValue(), rightX, y, true, Color.blue, Assets.getAssets().font14);
+            y += 64;
+        }
     }
-    
+
+    private void setKeySettings() 
+    {
+        keySettings = new LinkedHashMap<>();
+        
+        keySettings.put("Move", "WASD");
+        keySettings.put("Interact", "E");
+        keySettings.put("Attack", "X");
+        keySettings.put("Inventory", "I");
+        keySettings.put("Return to menu", "ESC");
+    }
 }
