b5f8eb50159b17797d4b6cca9dcc3154ef89237c
limmens <66556410+limmens@users.noreply.github.com>
2020-12-14 12:35:40 +0100

    Added simple save and load functionality

---
+++ src/tilegame/saves/ResourceManager.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tilegame.saves;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author Loes Immens, based on Almas Baimagambetov's Java Serialization (Save/Load data) video:
+ * https://www.youtube.com/watch?v=-xW0pBZqpjU
+ */
+public class ResourceManager 
+{
+    public static void save(Serializable data, String fileName) throws Exception
+    {
+        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName))))
+        {
+            oos.writeObject(data);
+        }
+    }
+    
+    public static Object load(String fileName) throws Exception
+    {
+        try(ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(fileName))))
+        {
+            return ois.readObject();
+        }
+    }
+}
